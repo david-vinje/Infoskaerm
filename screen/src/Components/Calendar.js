232f4a50174a04a55212b9fd078d100d8b73d10e
david-vinje <dnvinje@gmail.com>
2024-08-29 16:02:37 +0200

    omg it's happening

--- screen/src/Components/Calendar.js
+++ screen/src/Components/Calendar.js
@@ -51,9 +51,9 @@ const Countdown = ({ events }) => {
             : <></>
           }
         </div>
-        <div className="col-8 text-end pe-3">
+        <div className="col-8 text-end align-self-center pe-3">
             <p className="display-1">{daysUntil} </p>
-          <p className="pb-3 footer-text">Dage til: {event.text}</p>
+          <p className="footer-text">Dage til: {event.text}</p>
         </div>
       </Card.Body>
       {/* <Card.Footer className="d-flex p-2 justify-content-between">
@@ -161,14 +161,14 @@ const Percentage = ({ percentage }) => {
 
 const InfoCard = ({ title, heading, subheading, percentage, bannerText }) => (
   <Card className="h-100">
-    <Card.Body className="row p-3">
-      <div className="col-4 d-flex flex-column">
+    <Card.Body className="row center p-3">
+      <div className="col-4 d-flex flex-column ">
         <p className="info-text pb-2">{title}</p>
         <Percentage percentage={percentage} />
       </div>
       <div className="col-8 text-end">
         <p className="display-1">{heading}</p>
-        <p className="pb-3 footer-text">{subheading}</p>
+        <p className="footer-text">{subheading}</p>
       </div>
     </Card.Body>
     {/* <Card.Footer className="d-flex p-2 justify-content-between">
