c42a01c108cf6bfe32a6db07cee5beb660853cff
david-vinje <dnvinje@gmail.com>
2024-09-03 16:16:06 +0200

    infobox text dark

--- screen/src/Components/Calendar.js
+++ screen/src/Components/Calendar.js
@@ -58,7 +58,7 @@ const Infobox = ({ infobox }) => {
   console.log(infobox)
   if (!infobox) return
   return (
-    <div className="row text-light bg-blue border border-secondary mt-5 rounded w-100 mh-25 mx-auto p-4">
+    <div className="row bg-blue border border-secondary mt-5 rounded w-100 mh-25 mx-auto p-4">
         <p className="display-5">{getIcon[infobox.newsType]} {infobox.description}</p>
         <p className="text-description text-ellipsis-6">{infobox.text}</p>
     </div>
