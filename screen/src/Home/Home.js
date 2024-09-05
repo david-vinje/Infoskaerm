12a44adec03e598d730775b3f861d58bea9a1d6c
david-vinje <dnvinje@gmail.com>
2024-09-05 11:28:18 +0200

    fix carousel

--- screen/src/Home/Home.js
+++ screen/src/Home/Home.js
@@ -7,8 +7,8 @@ import { getProjects, getClientLogoUudid, getEmployeePhotoUuid, getConsultants, 
 import HomeCard from "./HomeCard";
 import Calendar from "../Components/Calendar";
 
-const INTERVAL = 1000
-const MILLISECONDS_PER_DAY = 24 * 60 * 60 * 1000
+const INTERVAL = 10000
+const REFRESH = 1000 * 60 * 60
 
 const Home = () => {
     const navigate = useNavigate();
@@ -22,7 +22,7 @@ const Home = () => {
     const [headcount, setHeadcount] = useState([]);
     const [isPortrait, setOrientation] = useState(window.matchMedia("(orientation: portrait)").matches)
 
-    setInterval(() => navigate(0), MILLISECONDS_PER_DAY / 12)
+    setInterval(() => navigate(0), REFRESH)
     window.addEventListener("resize", () => setOrientation(window.matchMedia("(orientation: portrait)").matches))
 
     useEffect(() => {
