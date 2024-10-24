e29881a48ab88b76066dc6ba952bd0be8b9d19eb
NAN005 <125190597+NAN005@users.noreply.github.com>
2024-10-24 11:31:21 +0200

    t

--- screen/src/Home/HomeCard.js
+++ screen/src/Home/HomeCard.js
@@ -11,13 +11,13 @@ const HomeCard = ({
   getEmployeePhoto,
 }) => (
   <div className="container-fluid d-flex flex-column"> {/*container-fluid = which is width: 100% at all breakpoints, d-flex = display flex container, flex-colum = set a vertical direction */}
-    <div className="row mx-auto mb-5 w-75 clientlogoborder rounded">
+    <div className="row row-1 mx-auto w-75 clientlogoborder clientLogo rounded"> {/* mb-5  */}
       <img
-        className="rounded p-0"
+        className=" rounded p-0"
         src={`data:image/jpeg;base64,${getClientLogo(project.clientuuid)}`}
       />
     </div>
-    <div className="row pt-5 mh-50 overflow-hidden">
+    <div className="row row-2 pt-5 mh-50 overflow-hidden">
       <h1 className="project-name pt-5 display-1">{project.name}</h1>
       <ProjectStatus project={project}></ProjectStatus>
       <div className="col-8 right-border lh-lg project-description">
@@ -56,7 +56,7 @@ const HomeCard = ({
         </div>
       </div>
     </div>
-    <div className="row pb-5 mt-auto">
+    <div className="row row-3 pb-5 mt-auto">
       {project.projectDescriptionUserList.slice(0, PEOPLE_LIMIT).map((user) => (
         <div className="col-2" key={user.useruuid}>
           <img
