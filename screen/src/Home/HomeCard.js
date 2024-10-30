482ee05897d58e5c313b0e67c1f12b62cf9869a4
NAN005 <125190597+NAN005@users.noreply.github.com>
2024-10-30 16:12:53 +0100

    skærmen

--- screen/src/Home/HomeCard.js
+++ screen/src/Home/HomeCard.js
@@ -52,7 +52,7 @@ const HomeCard = ({
         </div>
       </div>
     </div>
-    <div className="row row-3 mt-auto">
+    <div className="row mt-auto">
       {project.projectDescriptionUserList.slice(0, PEOPLE_LIMIT).map((user) => (
         <div className="col-2" key={user.useruuid}>
           <img
