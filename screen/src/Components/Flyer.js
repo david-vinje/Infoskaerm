37031a2b383072f60727583775b500d0773f7805
Adam Hoppe <adam.hoppe@trustworks.dk>
2025-02-19 13:57:32 +0100

    flyer height and center adjust

--- screen/src/Components/Flyer.js
+++ screen/src/Components/Flyer.js
@@ -2,17 +2,17 @@ import React from "react";
 
 
 
-const Flyer = ({content}) => {
-  return (
-      <div className="container-fluid">
-        <div className="">
-        <img
-        className="rounded p-0 h-75"
-        src={content}
-        />
+const Flyer = ({ content }) => {
+    return (
+        <div className="container-fluid center ">
+
+            <img
+                className="rounded p-0 h-100"
+                src={content}
+            />
+
         </div>
-      </div>
-    
-  )
+
+    )
 }
 export default Flyer
