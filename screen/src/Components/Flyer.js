cca0bc8c14f92d7d94c172a29e243bfa656d69e8
Adam Hoppe <adam.hoppe@trustworks.dk>
2025-02-19 15:03:12 +0100

    4 img instead of 1

--- screen/src/Components/Flyer.js
+++ screen/src/Components/Flyer.js
@@ -4,24 +4,17 @@ import React from "react";
 
 const Flyer = ({ content }) => {
     return (
-        <div className="container-fluid center ">
+        <div className="container-fluid center d-flex flex-column ">
+            {content.map((flyer, index) => (
+            <img
+                key={index}
+                style={{boxShadow: "0px 0px 20px 5px rgba(75, 61, 61, 0.3)"}}
+                className="rounded my-2 h-25"
+                src={flyer}
+            />
+            ))}
 
-            <img
-                className="rounded p-0 h-100"
-                src={content[0]}
-            />
-            <img
-                className="rounded p-0 h-100"
-                src={content[1]}
-            />
-            <img
-                className="rounded p-0 h-100"
-                src={content[2]}
-            />
-            <img
-                className="rounded p-0 h-100"
-                src={content[3]}
-            />
+
 
         </div>
 
--- screen/src/Home/Home.js
+++ screen/src/Home/Home.js
@@ -14,8 +14,13 @@ import {
 } from "../Components/API";
 import HomeCard from "./HomeCard";
 import Calendar from "../Components/Calendar";
-import Flyer from "../Components/Flyer";
 import IndustryInsights from "../Components/IndustryInsights";
+import Flyer from "../Components/Flyer"
+import ENPS from "../img/ENPS.png"
+import ENPS1 from "../img/ENPS1.png"
+import ENPS2 from "../img/ENPS2.png"
+import ENPS3 from "../img/ENPS3.png"
+import ENPS4 from "../img/ENPS4.png"
 
 const INTERVAL = 1000 * 30; // 30 seconds
 const CALENDAR_INTERVAL = 5; // every 5 slides
@@ -132,7 +137,7 @@ const Home = () => {
       fetchClientPhotos();
     }
   }, [activeProjects]);
-  
+
   //Function to get the employee photo
   function getEmployeePhoto(id) {
     const employee = employees.find(employee => employee.id === id);
@@ -160,24 +165,33 @@ const Home = () => {
       elem.click()
     }
   }
-  
+  let flyerCounter = 0;
   return (
     <Wrapper className="body::before">
       <Carousel onKeyDown={keyDown} id="carousel" data-wrap pause={false}>
         {activeProjects.map((project, index) => {
-          if (index % CALENDAR_INTERVAL === 0 && index % 2 === 1) {
-            return (
-              <Carousel.Item key={index} interval={INTERVAL * 2}>
-                <Calendar events={events} headcount={headcount} />
-              </Carousel.Item>
-            );
-          }
-          if (index % CALENDAR_INTERVAL === 0 && index % 2 === 0) {
-            return (
-              <Carousel.Item key={index} interval={INTERVAL * 2}>
-                <IndustryInsights content={coffeeMeetings} getEmployeePhoto={getEmployeePhoto} />
-              </Carousel.Item>
-            );
+          if (index % CALENDAR_INTERVAL === 0) {
+            if (index % 2 === 1) {
+              return (
+                <Carousel.Item key={index} interval={INTERVAL * 2}>
+                  <Calendar events={events} headcount={headcount} />
+                </Carousel.Item>
+              );
+            } else {
+              if (flyerCounter++ % 2 === 0) {
+                return (
+                  <Carousel.Item key={index} interval={INTERVAL * 2}>
+                    <IndustryInsights content={coffeeMeetings} getEmployeePhoto={getEmployeePhoto} />
+                  </Carousel.Item>
+                );
+              } else {
+                return (
+                  <Carousel.Item key={index} interval={INTERVAL * 2}>
+                    <Flyer content={[ENPS1, ENPS2, ENPS3, ENPS4]} />
+                  </Carousel.Item>
+                );
+              }
+            }
           }
           return (
             <Carousel.Item key={index} autoFocus interval={INTERVAL}>
