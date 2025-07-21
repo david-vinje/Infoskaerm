ed0e40e35fe6a5c9c278de54cb096bd1c78a0b8d
AdamHoppe98 <adamhoppe98@gmail.com>
2025-07-21 10:26:52 +0200

    fixed cors error

--- screen/src/Components/API.js
+++ screen/src/Components/API.js
@@ -11,7 +11,7 @@ export const config = {
 
 async function fetchHeadcount(date) {
   try {
-    const response = await fetch('https://api.trustworks.dk/public/stats/employees/headcount/' + date, config);
+    const response = await fetch('/public/stats/employees/headcount/' + date, config);
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
@@ -57,15 +57,15 @@ export const sortedEvents = events => {
 
 export async function getEvents(setEvents) {
   try {
-    console.log('process.env.REACT_APP_TOKEN', process.env.REACT_APP_TOKEN)
-    const response = fetch('https://api.trustworks.dk/public/news/office_display', {
+    const response = await fetch('/public/news/office_display', {
       method: 'GET',
       headers: {
         'Accept': 'application/json',
         'Authorization': `Bearer ${process.env.REACT_APP_TOKEN}`
       }
     })
-
+    
+    console.log('res', response)
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
@@ -83,7 +83,7 @@ export async function getEvents(setEvents) {
 
 export async function getProjects(setProjects) {
   try {
-    const response = await fetch('https://api.trustworks.dk/public/knowledge/projects/', config);
+    const response = await fetch(' /public/knowledge/projects/', config);
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
@@ -110,7 +110,7 @@ export async function getProjects(setProjects) {
 
 export async function getEmployeePhotoUuid(useruuid) {
   try {
-    const response = await fetch(`https://api.trustworks.dk/public/users/${useruuid}/photo/`, config);
+    const response = await fetch(`/public/users/${useruuid}/photo/`, config);
 
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
@@ -127,7 +127,7 @@ export async function getEmployeePhotoUuid(useruuid) {
 
 export async function getConsultants(setConsultants) {
   try {
-    const response = await fetch('https://api.trustworks.dk/public/users/', config);
+    const response = await fetch('/public/users/', config);
 
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
@@ -167,7 +167,7 @@ export function getActiveConsultants(consultants) {
 
 export async function getClients(setClients) {
   try {
-    const response = await fetch('https://api.trustworks.dk/public/clients/', config);
+    const response = await fetch('/public/clients/', config);
 
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
@@ -186,7 +186,7 @@ export async function getClients(setClients) {
 
 export async function getClientLogoUudid(clientuuid) {
   try {
-    const response = await fetch(`https://api.trustworks.dk/public/files/photos/${clientuuid}/`, config);
+    const response = await fetch(`/public/files/photos/${clientuuid}/`, config);
 
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
@@ -209,7 +209,7 @@ export async function updateClientListWithIdPhoto(projects, setClients) {
     await Promise.all(
       projects?.map(async (project) => {
         const response = await fetch(
-          `https://api.trustworks.dk/public/files/photos/${project.clientuuid}`,
+          `/public/files/photos/${project.clientuuid}`,
           config
         );
 
@@ -233,7 +233,7 @@ export async function updateClientListWithIdPhoto(projects, setClients) {
 
 export const getCoffeeMeetings = async (setCoffeeMeetings) => {
   // try {
-  //   const response = await fetch('https://api.trustworks.dk/public/foo/bar/coffeemeetings/' + date, config);
+  //   const response = await fetch(' /public/foo/bar/coffeemeetings/' + date, config);
   //   if (!response.ok) {
   //     throw new Error(`HTTP error! Status: ${response.status}`);
   //   }
