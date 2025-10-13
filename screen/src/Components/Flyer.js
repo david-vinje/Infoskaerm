e09554a08645663ecade70ce4b6e5c559e574268
david-vinje <dnvinje@gmail.com>
2025-10-13 15:35:57 +0200

    more flyer

--- screen/src/Components/Flyer.js
+++ screen/src/Components/Flyer.js
@@ -7,7 +7,7 @@ const Flyer = ({ content }) => {
             <img
                 key={index}
                 style={{boxShadow: "0px 0px 20px 5px rgba(75, 61, 61, 0.3)"}}
-                className="rounded my-2 h-75 w-75"
+                className="rounded my-2 h-100 w-100"
                 src={flyer}
             />
             ))}
